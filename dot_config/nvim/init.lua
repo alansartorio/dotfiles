56aa4c9739b3641c448b0961ed2e7da30d37fa38
Alan Gabriel Sartorio <alan42ga@hotmail.com>
2026-07-15 09:54:01 -0300

    replace nvim lua function

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -687,7 +687,7 @@ vim.lsp.config("roslyn", {
 	cmd = {
 		"Microsoft.CodeAnalysis.LanguageServer",
 		"--logLevel=Information",
-		"--extensionLogDirectory=" .. vim.fs.dirname(vim.lsp.get_log_path()),
+		"--extensionLogDirectory=" .. vim.fs.dirname(vim.lsp.log.get_filename()),
 		"--stdio",
 	},
 })
